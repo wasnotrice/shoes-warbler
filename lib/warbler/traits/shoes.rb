586d4aa1cef9771b39c965a2f4a9fb4e64341c26
Eric Watson <wasnotrice@gmail.com>
2012-10-10 06:29:23 -0500

    Move Warbler config into Shoes JAR config

--- lib/warbler/traits/shoes.rb
+++ lib/warbler/traits/shoes.rb
@@ -17,17 +17,6 @@ module Warbler
         [Traits::Jar, Traits::NoGemspec].include? trait
       end
 
-      def before_configure
-        custom_config = YAML.load(File.read 'app.yaml')
-        @app_config = ::Shoes::Package::Configuration.new(custom_config)
-        config.jar_name = @app_config.shortname
-        config.pathmaps.application = ['shoes-app/%p']
-        specs = @app_config.gems.map { |g| Gem::Specification.find_by_name(g) }
-        dependencies = specs.map { |s| s.runtime_dependencies }.flatten
-        (specs + dependencies).uniq.each { |g| config.gems << g }
-        config.excludes += FileList[*@app_config.ignore].pathmap(config.pathmaps.application.first)
-      end
-
       def after_configure
         config.init_contents << StringIO.new("require 'shoes'\nShoes.configuration.backend = :swt\n")
       end
@@ -38,14 +27,14 @@ module Warbler
         add_main_rb(jar, apply_pathmaps(config, default_executable, :application))
       end
 
-      # Uses the `@app_config.run` if it exists. Otherwise, looks in the
+      # Uses the `@config.run` if it exists. Otherwise, looks in the
       # application's `bin` directory for an executable with the same name as
       # the jar. If this also fails, defaults to the first executable (alphabetically) in the
       # applications `bin` directory.
       #
       # @return [String] filename of the executable to run
       def default_executable
-        return @app_config.run.split('\s').first if @app_config.run
+        return @config.run if @config.run
         exes = Dir['bin/*'].sort
         exe = exes.grep(/#{config.jar_name}/).first || exes.first
         raise "No executable script found" unless exe
